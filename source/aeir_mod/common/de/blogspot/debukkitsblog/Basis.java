5cf1287bcbf3c70b9e45c8bff0af38cfbabaf198
Lars Moellendorf <moellendorf@phaenovum.de>
2013-10-04 16:58:04 +0200

    added first unordered crafting recipe

--- source/aeir_mod/common/de/blogspot/debukkitsblog/Basis.java
+++ source/aeir_mod/common/de/blogspot/debukkitsblog/Basis.java
@@ -1,5 +1,8 @@
 package de.blogspot.debukkitsblog;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
@@ -10,6 +13,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.GameRegistry;
 import de.blogspot.debukkitsblog.proxies.BasisCommonProxy;
 
 @Mod(modid = "basis", name = "Basis Mod", version = "1.0.0")
@@ -29,11 +33,25 @@ public class Basis {
 
 	@Init
 	public void init(FMLInitializationEvent event) {
-
+		initCraftingRecipes();
 	}
 
 	@PostInit
 	public static void postInit(FMLPostInitializationEvent event) {
 
+	}
+
+	/*
+	 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	 * private methods
+	 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	 */
+
+	/**
+	 * add crafting recipes
+	 */
+	private void initCraftingRecipes() {
+		GameRegistry.addShapelessRecipe(new ItemStack(Item.diamond, 64),
+				new ItemStack(Block.dirt));
 	}
 }
